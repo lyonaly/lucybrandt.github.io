8cc7e07294454bf81055df60735855ddc99a02b0
lyybt <Lucy.brandt.mh@gmail.com>
2026-01-03 13:24:45 +0100

    automatic  age

--- src/pages/MainView.jsx
+++ src/pages/MainView.jsx
@@ -3,14 +3,26 @@ import Certificates from '../components/Certificates';
 import Experiences from '../components/Experiences';
 
 export default function MainView({ lang, aboutRef, experienceRef, skillsRef, certificatesRef, showDetails }) {
-    const heding = {
+
+    const birthDate = new Date(2004, 3);
+
+    const getAge = date => {
+        const today = new Date();
+        let age = today.getFullYear() - date.getFullYear();
+        if (today.getMonth() < date.getMonth()) age--;
+        return age;
+    };
+
+    const age = getAge(birthDate);
+
+    const heading = {
         de: 'Hi, ich bin',
         en: 'Hi, I am'
     };
 
     const texts = {
-        de: "Ich bin 21 Jahre alt und studiere Software Engineering in Essen. Mich begeistert die Frontend-Entwicklung, da sie technisches Denken mit kreativem Gestalten verbindet, insbesondere die Umsetzung performanter, moderner und intuitiver Nutzeroberflächen. Auf dieser Seite erhältst du einen Einblick in meinen Werdegang sowie meine technischen Schwerpunkte.",
-        en: "I'm 21 years old and studying software engineering in Essen. I'm particularly passionate about frontend development, as it combines technical thinking with creative design, especially the implementation of high-performance, modern, and intuitive user interfaces. On this page, you will find a detailed overview of my background as well as my technical focus."
+        de: `Ich bin ${age} Jahre alt und studiere Software Engineering in Essen. Mich begeistert die Frontend-Entwicklung, da sie technisches Denken mit kreativem Gestalten verbindet, insbesondere die Umsetzung performanter, moderner und intuitiver Nutzeroberflächen. Auf dieser Seite erhältst du einen Einblick in meinen Werdegang sowie meine technischen Schwerpunkte.`,
+        en: `I'm ${age} years old and studying software engineering in Essen. I'm particularly passionate about frontend development, as it combines technical thinking with creative design, especially the implementation of high-performance, modern, and intuitive user interfaces. On this page, you will find a detailed overview of my background as well as my technical focus.`
     };
 
     return (
@@ -18,7 +30,7 @@ export default function MainView({ lang, aboutRef, experienceRef, skillsRef, cer
             <section id="about" className="aboutme" ref={aboutRef}>
                 <img src="photo.png" className="photo" alt="Lucy" />
                 <article className="text-aboutme">
-                    <h1 id="heading">{heding[lang]}</h1>
+                    <h1 id="heading">{heading[lang]}</h1>
                     <h1>Lucy Brandt</h1>
                     <p id="description" className="descr">{texts[lang]}</p>
                 </article>
