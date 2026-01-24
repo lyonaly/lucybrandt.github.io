4901ba6ebc54b7c9cdc7dcb15076b20a4b05514d
lyybt <Lucy.brandt.mh@gmail.com>
2026-01-24 10:48:03 +0100

    impressum

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,13 @@ export default function Footer({ lang, footerRef }) {
         <footer ref={footerRef}>
             <div className="footer-content">
                 <div className="footcontainer">
-                    <h2>{lang === 'de' ? 'Kontakt' : 'Contact'}</h2>
-                    <p>Email: Lucy.brandt.dev@gmail.com</p>
+                    <h2>{lang === 'de' ? 'Kontakt & Impressum' : 'Contact & Imprint'}</h2>
+                    <p>E-mail: Lucy.brandt.dev@gmail.com</p>
                     <p>Linkedin:
                         <a className="footer-link" href="https://www.linkedin.com/in/lucy-brandt-dev/" target="_blank" rel="noreferrer"> https://www.linkedin.com/in/lucy-brandt-dev/</a>
                     </p>
+                    <p>{lang === 'de' ? 'Anschrift: ' : 'Address: '}Lucy Brandt, Luxemburger Allee 38, 45481 Mülheim an der Ruhr, Deutschland</p>
+                    <p>{lang === 'de' ? 'Telefon: ' : 'Phone: '}+49 157 56313807</p>
                 </div>
                 <p className="copyright">{lang === 'de' ? '© 2025 Lucy Brandt. Alle Rechte vorbehalten.' : '© 2025 Lucy Brandt. All rights reserved.'}</p>
             </div>
